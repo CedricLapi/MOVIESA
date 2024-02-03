cfbbf1367d7936d0600e369b84c7030a0238dc43
Cedric Lapi <lapicedric@gmail.com>
2024-02-03 13:54:39 +0100

    backend done!

--- SERVER/controllers/movie.controller.js
+++ SERVER/controllers/movie.controller.js
@@ -4,7 +4,7 @@ module.exports.findAllMovies = (req, res) => {
     Movie.find()
         .then(allMovies => res.json({ movies: allMovies }))
         .catch(err => res.json({ message: 'Something went wrong', error: err}));
-}
+},
 
 module.exports.create = (req, res) => {
     const { title, genre, year } = req.body;
@@ -16,4 +16,47 @@ module.exports.create = (req, res) => {
         .then(newMovie => res.json({ movie: newMovie }))
         .catch(err => res.json({ message: 'Something went wrong', error: err}));
 
+},
+
+module.exports.findByGenre = (req, res) => {
+    Movie.find({ genre: req.params.genre.toLowerCase() })
+    .then(movie => res.json({ movie }))
+    .catch(err => res.json({ message: 'Something went wrong', error: err }));
+},
+
+//would give the first movie of the genre
+module.exports.findByGenreOne = (req, res) => {
+    Movie.findOne({ genre: req.params.genre.toLowerCase() })
+    .then(movie => res.json({ movie }))
+    .catch(err => res.json({ message: 'Something went wrong', error: err }));
+},
+
+module.exports.findOneById = (req, res) => {
+    Movie.findById({ _id: req.params.id })
+    .then(movie => res.json({ movie }))
+    .catch(err => res.json({ message: 'Something went wrong', error: err }));
+},
+
+//Update one Movie by ID with PUT
+/*module.exports.updateMovie = (req, res) => {
+    Movie.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+        .then(updatedMovie => res.json({ movie: updatedMovie }))
+        .catch(err => res.json({ message: 'Something went wrong', error: err}));
+},*/
+
+//Updtae one Movie by ID with PATCH
+
+module.exports.updateMovie = (req, res) => {
+    Movie.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+    .then(updatedMovie => res.json({ movie: updatedMovie }))
+    .catch(err => res.json({ message: 'Something went wrong', error: err }));
+},
+
+module.exports.deleteMovie = (req, res) => {
+    Movie.findByIdAndDelete({_id: req.params.id})
+    .then(deleteConfirmation => res.json({ deleteConfirmation }))
+    .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
+
+
+
--- SERVER/routes/movie.routes.js
+++ SERVER/routes/movie.routes.js
@@ -1,6 +1,20 @@
 const MovieController = require('../controllers/movie.controller');
 
 module.exports = app => {
+
     app.get('/api/movies', MovieController.findAllMovies);
+
+    app.get('/api/movies/:genre', MovieController.findByGenre);
+    
+    app.get('/api/movies/one_movie/:id', MovieController.findOneById);
+
+    app.get('/api/movies/just_first/:genre', MovieController.findByGenreOne);
+
     app.post('/api/movies', MovieController.create);
+
+    app.patch('/api/movies/update/:id', MovieController.updateMovie);
+
+    app.delete('/api/movies/delete/:id', MovieController.deleteMovie);
+
+    //app.put('/api/movies/update/:id', MovieController.updateMovie);
 }
